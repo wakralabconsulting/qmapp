13f1784eb042a7c86cf79afd12c736df5a557dc2
Vidya R <vidyar851@gmail.com>
2018-10-09 10:50:44 +0530

    Added Audio in FloorMap

--- QatarMuseum/ViewController/FloorMapViewController.swift
+++ QatarMuseum/ViewController/FloorMapViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import Alamofire
+import AVFoundation
+import AVKit
 import GoogleMaps
 import Crashlytics
 import UIKit
@@ -38,6 +40,9 @@ class FloorMapViewController: UIViewController, GMSMapViewDelegate, ObjectPopUpP
     @IBOutlet weak var thirdLevelLabel: UILabel!
     @IBOutlet weak var levelView: UIView!
     @IBOutlet weak var numberSerchBtn: UIButton!
+    @IBOutlet weak var overlayView: UIView!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var playerSlider: UISlider!
     
     var bottomSheetVC:MapDetailView = MapDetailView()
     var floorMapArray: [TourGuideFloorMap]! = []
@@ -47,11 +52,13 @@ class FloorMapViewController: UIViewController, GMSMapViewDelegate, ObjectPopUpP
     var selectedTourdGuidIndex : Int? = 0
     var selectedMarker = GMSMarker()
     var selectedMarkerImage = UIImage()
-    
     var bounceTimerTwo = Timer()
     var bounceTimerThree = Timer()
-    
-    @IBOutlet weak var overlayView: UIView!
+    var playList: String = ""
+    var timer: Timer?
+    var avPlayer: AVPlayer!
+    var isPaused: Bool!
+    var firstLoad: Bool = true
     
     
     
@@ -189,6 +196,8 @@ class FloorMapViewController: UIViewController, GMSMapViewDelegate, ObjectPopUpP
         loadMap()
         initialSetUp()
         getFloorMapDataFromServer()
+        print(UIScreen.main.bounds.width)
+        print(UIScreen.main.bounds.height)
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(false)
@@ -222,15 +231,24 @@ class FloorMapViewController: UIViewController, GMSMapViewDelegate, ObjectPopUpP
         thirdLevelView.layer.shadowRadius = 1
         if (fromScienceTour == true) {
             
-            
+            if (selectedScienceTourLevel == "1"){
+                playButton.isHidden = true
+                playerSlider.isHidden = true
+            }
             if(selectedScienceTourLevel == "2") {
                 firstLevelView.backgroundColor = UIColor.mapLevelColor
                 secondLevelView.backgroundColor = UIColor.white
                 thirdLevelView.backgroundColor = UIColor.mapLevelColor
+                
+                playButton.isHidden = false
+                playerSlider.isHidden = false
             } else {
                 firstLevelView.backgroundColor = UIColor.mapLevelColor
                 secondLevelView.backgroundColor = UIColor.mapLevelColor
                 thirdLevelView.backgroundColor = UIColor.white
+                
+                playButton.isHidden = false
+                playerSlider.isHidden = false
             }
             
             
@@ -265,6 +283,8 @@ class FloorMapViewController: UIViewController, GMSMapViewDelegate, ObjectPopUpP
         headerView.headerViewDelegate = self
         headerView.headerTitle.text = NSLocalizedString("FLOOR_MAP_TITLE", comment: "FLOOR_MAP_TITLE  in the Floormap page")
         
+        
+    
     }
     
     func loadMap() {
@@ -710,6 +730,13 @@ class FloorMapViewController: UIViewController, GMSMapViewDelegate, ObjectPopUpP
     
     //MARK: Floor Levels
     @IBAction func didTapThirdLevel(_ sender: UIButton) {
+        playButton.isHidden = false
+        playerSlider.isHidden = false
+        firstLoad = true
+        self.avPlayer = nil
+        self.timer?.invalidate()
+        playerSlider.value = 0
+        playButton.setImage(UIImage(named:"play_blackX1"), for: .normal)
         level = levelNumber.three
         firstLevelView.backgroundColor = UIColor.mapLevelColor
         secondLevelView.backgroundColor = UIColor.mapLevelColor
@@ -723,6 +750,13 @@ class FloorMapViewController: UIViewController, GMSMapViewDelegate, ObjectPopUpP
     }
     
     @IBAction func didtapSecondbutton(_ sender: UIButton) {
+        playButton.isHidden = false
+        playerSlider.isHidden = false
+        firstLoad = true
+        self.avPlayer = nil
+        self.timer?.invalidate()
+        playerSlider.value = 0
+        playButton.setImage(UIImage(named:"play_blackX1"), for: .normal)
         level = levelNumber.two
         firstLevelView.backgroundColor = UIColor.mapLevelColor
         secondLevelView.backgroundColor = UIColor.white
@@ -735,6 +769,13 @@ class FloorMapViewController: UIViewController, GMSMapViewDelegate, ObjectPopUpP
     }
     
     @IBAction func didTapFirstButton(_ sender: UIButton) {
+        playButton.isHidden = true
+        playerSlider.isHidden = true
+        firstLoad = true
+        self.avPlayer = nil
+        self.timer?.invalidate()
+        playerSlider.value = 0
+        playButton.setImage(UIImage(named:"play_blackX1"), for: .normal)
         level = levelNumber.one
         firstLevelView.backgroundColor = UIColor.white
         secondLevelView.backgroundColor = UIColor.mapLevelColor
@@ -923,6 +964,124 @@ class FloorMapViewController: UIViewController, GMSMapViewDelegate, ObjectPopUpP
             showLevelThreeMarker()
         }
     }
+    //MARK: Audio SetUp
+    func play(url:URL) {
+        self.avPlayer = AVPlayer(playerItem: AVPlayerItem(url: url))
+        if #available(iOS 10.0, *) {
+            self.avPlayer.automaticallyWaitsToMinimizeStalling = false
+        }
+        avPlayer!.volume = 1.0
+        avPlayer.play()
+    }
+    @IBAction func playButtonClicked(_ sender: UIButton) {
+        if (firstLoad == true) {
+            self.playList = "http://www.qm.org.qa/sites/default/files/floors.mp3"
+            self.play(url: URL(string:self.playList)!)
+            self.setupTimer()
+        }
+        firstLoad = false
+        if #available(iOS 10.0, *) {
+            self.togglePlayPause()
+        } else {
+            // showAlert "upgrade ios version to use this feature"
+            
+        }
+    }
+    @available(iOS 10.0, *)
+    func togglePlayPause() {
+        if avPlayer.timeControlStatus == .playing  {
+            playButton.setImage(UIImage(named:"play_blackX1"), for: .normal)
+            avPlayer.pause()
+            isPaused = true
+        } else {
+            playButton.setImage(UIImage(named:"pause_blackX1"), for: .normal)
+            avPlayer.play()
+            isPaused = false
+        }
+    }
+    @IBAction func sliderValueChange(_ sender: UISlider) {
+//        let seconds : Int64 = Int64(sender.value)
+//        let targetTime:CMTime = CMTimeMake(seconds, 1)
+//        avPlayer!.seek(to: targetTime)
+//        if(isPaused == false){
+//            //seekLoadingLabel.alpha = 1
+//        }
+    }
+//    @IBAction func sliderTapped(_ sender: UILongPressGestureRecognizer) {
+//        if let slider = sender.view as? UISlider {
+//            if slider.isHighlighted { return }
+//            let point = sender.location(in: slider)
+//            let percentage = Float(point.x / slider.bounds.width)
+//            let delta = percentage * (slider.maximumValue - slider.minimumValue)
+//            let value = slider.minimumValue + delta
+//            slider.setValue(value, animated: false)
+//            let seconds : Int64 = Int64(value)
+//            let targetTime:CMTime = CMTimeMake(seconds, 1)
+//            avPlayer!.seek(to: targetTime)
+//            if(isPaused == false){
+//               // seekLoadingLabel.alpha = 1
+//            }
+//        }
+//    }
+    func setupTimer(){
+        NotificationCenter.default.addObserver(self, selector: #selector(self.didPlayToEnd), name: .AVPlayerItemDidPlayToEndTime, object: nil)
+        timer = Timer(timeInterval: 0.001, target: self, selector: #selector(FloorMapViewController.tick), userInfo: nil, repeats: true)
+        RunLoop.current.add(timer!, forMode: RunLoopMode.commonModes)
+    }
+    @objc func didPlayToEnd() {
+       // self.nextTrack()
+    }
+    
+    @objc func tick(){
+        if(avPlayer.currentTime().seconds == 0.0){
+           // loadingLabel.alpha = 1
+        }else{
+            //loadingLabel.alpha = 0
+        }
+        
+        if(isPaused == false){
+            if(avPlayer.rate == 0){
+                avPlayer.play()
+               // seekLoadingLabel.alpha = 1
+            }else{
+                //seekLoadingLabel.alpha = 0
+            }
+        }
+        
+        if((avPlayer.currentItem?.asset.duration) != nil){
+            let currentTime1 : CMTime = (avPlayer.currentItem?.asset.duration)!
+            let seconds1 : Float64 = CMTimeGetSeconds(currentTime1)
+            let time1 : Float = Float(seconds1)
+            playerSlider.minimumValue = 0
+            playerSlider.maximumValue = time1
+            let currentTime : CMTime = (self.avPlayer?.currentTime())!
+            let seconds : Float64 = CMTimeGetSeconds(currentTime)
+            let time : Float = Float(seconds)
+            self.playerSlider.value = time
+           // timeLabel.text =  self.formatTimeFromSeconds(totalSeconds: Int32(Float(Float64(CMTimeGetSeconds((self.avPlayer?.currentItem?.asset.duration)!)))))
+            //currentTimeLabel.text = self.formatTimeFromSeconds(totalSeconds: Int32(Float(Float64(CMTimeGetSeconds((self.avPlayer?.currentItem?.currentTime())!)))))
+            
+        }else{
+            playerSlider.value = 0
+            playerSlider.minimumValue = 0
+            playerSlider.maximumValue = 0
+            //timeLabel.text = "Live stream \(self.formatTimeFromSeconds(totalSeconds: Int32(CMTimeGetSeconds((avPlayer.currentItem?.currentTime())!))))"
+        }
+    }
+    func formatTimeFromSeconds(totalSeconds: Int32) -> String {
+        let seconds: Int32 = totalSeconds%60
+        let minutes: Int32 = (totalSeconds/60)%60
+        let hours: Int32 = totalSeconds/3600
+        return String(format: "%02d:%02d:%02d", hours,minutes,seconds)
+    }
+    
+    @IBAction func backButtonClicked(_ sender: Any) {
+        self.dismiss(animated: true) {
+            self.avPlayer = nil
+            self.timer?.invalidate()
+        }
+    }
+    
     //MARK: WebServiceCall
     func getFloorMapDataFromServer()
     {
@@ -953,4 +1112,9 @@ class FloorMapViewController: UIViewController, GMSMapViewDelegate, ObjectPopUpP
     
     
     
+}
+extension AVPlayer {
+    var isPlaying: Bool {
+        return rate != 0 && error == nil
+    }
 }
