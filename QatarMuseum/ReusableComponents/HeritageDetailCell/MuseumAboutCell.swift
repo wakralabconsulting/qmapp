b5c48e6a26363a7233edbdd3be7aba23aa2fc35a
anjusr <anjusr5@gmail.com>
2018-11-30 21:18:32 +0530

    travel detail ui

--- QatarMuseum/ReusableComponents/HeritageDetailCell/MuseumAboutCell.swift
+++ QatarMuseum/ReusableComponents/HeritageDetailCell/MuseumAboutCell.swift
@@ -47,12 +47,15 @@ class MuseumAboutCell: UITableViewCell,iCarouselDelegate,iCarouselDataSource {
     @IBOutlet weak var videoOuterViewHeight: NSLayoutConstraint!
     @IBOutlet weak var downloadView: UIView!
     @IBOutlet weak var downloadImg: UIImageView!
-    
     @IBOutlet weak var downloadLabel: UILabel!
-    
     @IBOutlet weak var downloadButton: UIButton!
     @IBOutlet weak var downloadViewHeight: NSLayoutConstraint!
-    
+    @IBOutlet weak var offerCodeTitleLabel: UILabel!
+    @IBOutlet weak var codeLabel: UILabel!
+    @IBOutlet weak var claimOfferButton: UIButton!
+    @IBOutlet weak var offerCodeViewHeight: NSLayoutConstraint!
+    @IBOutlet weak var mapViewHeight: NSLayoutConstraint!
+
     var imgArray = NSArray()
     var favBtnTapAction : (()->())?
     var shareBtnTapAction : (()->())?
@@ -132,6 +135,7 @@ class MuseumAboutCell: UITableViewCell,iCarouselDelegate,iCarouselDataSource {
         contactTitleLabel.font = UIFont.closeButtonFont
         contactLabel.font = UIFont.sideMenuLabelFont
         favoriteBtnViewHeight.constant = 0
+        offerCodeViewHeight.constant = 0
         downloadLabel.font = UIFont.downloadLabelFont
     }
     
@@ -290,9 +294,8 @@ class MuseumAboutCell: UITableViewCell,iCarouselDelegate,iCarouselDataSource {
                 self.loadVideo(urlString: aboutData.multimediaVideo?[0])
             }
         }
-        
-       
     }
+    
     func setNMoQAboutCellData(aboutData: Museum) {
         middleTitleLabel.isHidden = false
         midTitleDescriptionLabel.isHidden = false
@@ -380,6 +383,60 @@ class MuseumAboutCell: UITableViewCell,iCarouselDelegate,iCarouselDataSource {
         
     }
     
+    func setNMoQTravelCellData(title: String) {
+        middleTitleLabel.isHidden = true
+        midTitleDescriptionLabel.isHidden = true
+        middleLabelLine.isHidden = true
+//        openingTimeTitleLabel.isHidden = true
+//        openingTimeLine.isHidden = true
+//        sundayTimeLabel.isHidden = true
+//        fridayTimeLabel.isHidden = true
+//        contactTitleLabel.isHidden = true
+//        contactLine.isHidden = true
+//        contactLabel.isHidden = true
+        
+//        middleTitleLabel.isHidden = false
+//        midTitleDescriptionLabel.isHidden = false
+//        middleLabelLine.isHidden = false
+        openingTimeTitleLabel.isHidden = false
+        openingTimeLine.isHidden = false
+        sundayTimeLabel.isHidden = false
+        fridayTimeLabel.isHidden = false
+        contactTitleLabel.isHidden = false
+        contactLabel.isHidden = false
+        subTitleLabel.isHidden = true
+        titleLabel.text = title
+        titleDescriptionLabel.text = "We have arranged for exclusive discounts for you. Just click on 'Claim Offers' button and use the code provided below"
+
+        fridayLabel.isHidden = true
+        locationFirstLabelHeight.constant = 0
+        downloadViewHeight.constant = 0
+        mapViewHeight.constant = 0
+        offerCodeViewHeight.constant = 200.0
+        downloadView.isHidden = true
+        downloadImg.isHidden = true
+        downloadLabel.isHidden = true
+        downloadButton.isHidden = true
+        
+        sundayTimeLabel.text = "+974 4452 5555"
+        
+        titleLabel.font = UIFont.closeButtonFont
+//        middleTitleLabel.font = UIFont.closeButtonFont
+//        locationTitleLabel.text = NSLocalizedString("LOCATION_TITLE",
+//                                                    comment: "LOCATION_TITLE in the Heritage detail")
+        locationTitleLabel.isHidden = true
+        openingTimeTitleLabel.text = NSLocalizedString("EVENT_DATE",
+                                                       comment: "EVENT_DATE in the Heritage detail")
+            contactTitleLabel.text = NSLocalizedString("CONTACT_TITLE",
+                                                       comment: "CONTACT_TITLE in the Heritage detail")
+            contactLabel.text = "info@qm.org.qa"
+            contactLine.isHidden = false
+//        }
+        
+        
+        
+        
+    }
 
     @IBAction func didTapFavouriteButton(_ sender: UIButton) {
         UIButton.animate(withDuration: 0.3,
